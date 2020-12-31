a0801fc7da90b08267b26884e15e0762bcd4e06e
Naomi Flagg <neffly511@gmail.com>
2020-12-31 12:46:42 -0800

    Add README and add additional notation to test script

---
+++ README.md
@@ -0,0 +1,9 @@
+## Balanced Binary Search Tree
+
+Ruby-based implementation of a balanced binary search tree, developed as part of The Odin Project's [Ruby on Rails Curriculum](http://www.theodinproject.com).
+
+## Features
+Methods include insertion, deletion, search, level order traversa and depth first traversals (inorder, preorder and postorder).
+
+## Screenshot demo
+![Screenshot](screenshot.png)
--- test_script.rb
+++ test_script.rb
@@ -1,22 +1,31 @@
 require_relative('tree.rb')
 
+puts 'Creating a tree from an array of random integers...'
 tree = Tree.new(Array.new(15) { rand(1..100) })
+puts 'Look, a balanced tree!'
 tree.pretty_print
+puts 'But is my tree balanced? True or false:'
 puts tree.balanced?
-puts "level order: #{tree.level_order}"
-puts "level order rec: #{tree.level_order_rec}"
-puts "inorder: #{tree.inorder}"
-puts "preorder: #{tree.preorder}"
-puts "postorder: #{tree.postorder}"
+puts "Traversing (or shall we say treeversing) level order: #{tree.level_order}"
+puts "Traversing level order, this time using recursion: #{tree.level_order_rec}"
+puts "Traversing inorder: #{tree.inorder}"
+puts "Traversing preorder: #{tree.preorder}"
+puts "Traversing postorder: #{tree.postorder}"
+puts "Let's add a few nodes... 182, 101 and 104."
 tree.insert(182)
 tree.insert(101)
 tree.insert(104)
+puts 'Is my tree balanced after adding nodes? True or false:'
 puts tree.balanced?
+puts 'Okay, rebalancing.'
 tree.rebalance
+puts 'Now is my tree balanced? True or false:'
 puts tree.balanced?
+puts "Let's do a few more traversals:"
 puts "level order: #{tree.level_order}"
 puts "level order rec: #{tree.level_order_rec}"
 puts "inorder: #{tree.inorder}"
 puts "preorder: #{tree.preorder}"
 puts "postorder: #{tree.postorder}"
+puts 'And one more pretty tree for the road!'
 tree.pretty_print
